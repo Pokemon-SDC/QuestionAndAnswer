4a66ef999814d0844525f69e23481588dcc94958
Brandon Hsu <brandon.j.hsu@gmail.com>
2022-07-30 15:50:39 -0700

    ensured that the queries are accurate in the PostgreSQL terminal with seeded data

--- server/index.js
+++ server/index.js
@@ -11,9 +11,6 @@ app.use(express.json());
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: true }));
 
-// list all the HTTP requests here, replace callback function with a
-// function from the controller file
-
 // POST routes
 app.post('/qa/questions', controller.addQuestion);
 app.post('/qa/questions/:question_id/answers', controller.addAnswer);
@@ -21,7 +18,6 @@ app.post('/qa/questions/:question_id/answers', controller.addAnswer);
 // GET routes
 app.get('/qa/questions', controller.getQuestions);
 app.get('/qa/questions/:question_id/answers', controller.getAnswers);
-// app.get('/photo', controller.getPhotos);
 
 // PUT routes
 app.put('/qa/questions/:question_id/helpful', controller.helpfulQuestion);
--- server/model.js
+++ server/model.js
@@ -10,24 +10,68 @@ const pool = require('../pool.js');
 */
 
 module.exports = {
+  getQuestions(product_id, page, count) {
+    return new Promise((resolve, reject) => {
+      const pageNumber = page || 1;
+      const countNumber = count || 5;
+      const productID = product_id;
+      const offset = (pageNumber - 1) * countNumber;
+
+      const data = {
+        product_id: productID,
+        results: [],
+      };
+
+      const queryStatement = `SELECT question.id, question.body, question.date_written, question.asker_name, question.helpfulness, question.reported,
+      (SELECT (json_object_agg(answer.answer_id, json_build_object('id', answer.answer_id, 'body', answer.body, 'date', answer.date_written, 'answerer_name', answer.answerer_name, 'helpfulness', answer.helpful,
+      'photos', (SELECT (array_agg(json_build_object('id', photo.id, 'photo_url', photo.photo_url))) FROM photo WHERE answer.answer_id = photo.answer_id))))
+      FROM answer WHERE answer.question_id = question.id)
+      AS answers
+      FROM question
+      WHERE question.product_id = ${productID} AND question.reported = false
+      ORDER BY question.question_helpfulness
+      DESC LIMIT ${countNumber}
+      OFFSET ${offset};`;
+
+      pool.query(queryStatement, (err, results) => {
+        if (err) {
+          reject(err);
+        }
+        data.results = results.rows;
+        resolve(data);
+      });
+    });
+  },
+
   getAnswers(question_id, page, count) {
     return new Promise((resolve, reject) => {
       const pageNumber = page || 1;
       const countNumber = count || 5;
+      const questionID = question_id;
+      const offset = (pageNumber - 1) * countNumber;
 
-      let data = {
+      const data = {
         question: question_id,
         page: pageNumber,
         count: countNumber,
         result: [],
       };
+
+      const queryStatement = `SELECT answer.answer_id, answer.body, answer.date_written, answer.answerer_name, answer.helpful, (SELECT (array_agg(json_build_object('id', photo.id, 'url', photo.photo_url))) FROM photo WHERE answer.answer_id = photo.answer_id) AS photos FROM answer WHERE answer.reported = false AND answer.question_id = ${questionID}, ORDER BY DESC LIMIT ${countNumber} OFFSET ${offset};`;
+      pool.query(queryStatement, (err, results) => {
+        if (err) {
+          reject(err);
+        }
+        data.results = results.rows;
+        resolve(data);
+      });
     });
   },
 
   addQuestion(product_id, body, name, email) {
     return new Promise((resolve, reject) => {
       const dateWritten = Math.floor(new Date().getTime());
-      const queryStatement = 'INSERT INTO question (product_id, body, date_written, asker_name, asker_email, reported, helpful VALUES (?, ?, ?, ?, ?, ?, ?)';
+      const queryStatement = 'INSERT INTO question (product_id, body, date_written, asker_name, asker_email, reported, helpfulness) VALUES (?, ?, ?, ?, ?, ?, ?);';
       const queryArguments = [product_id, body, dateWritten, name, email, 0, 0];
       pool.query(queryStatement, queryArguments, (err, results) => {
         if (err) {
@@ -41,7 +85,7 @@ module.exports = {
   addAnswer(question_id, body, name, email) {
     return new Promise((resolve, reject) => {
       const dateWritten = Math.floor(new Date().getTime());
-      const queryStatement = 'INSERT INTO answer(question_id, body, date_written, answerer_name, answerer_email, reported, helpful) VALUES (?, ?, ?, ?, ?, ?, ?)';
+      const queryStatement = 'INSERT INTO answer(question_id, body, date_written, answerer_name, answerer_email, reported, helpful) VALUES (?, ?, ?, ?, ?, ?, ?);';
       const queryArguments = [question_id, body, dateWritten, name, email, 0, 0];
       pool.query(queryStatement, queryArguments, (err, results) => {
         if (err) {
@@ -78,7 +122,7 @@ module.exports = {
 
   reportQuestion(question_id) {
     return new Promise((resolve, reject) => {
-      const queryStatement = `UPDATE question SET reported = 1 WHERE id = ${question_id};`;
+      const queryStatement = `UPDATE question SET reported = true WHERE id = ${question_id};`;
       pool.query(queryStatement, (err, results) => {
         if (err) {
           reject(err);
@@ -90,7 +134,7 @@ module.exports = {
 
   reportAnswer(answer_id) {
     return new Promise((resolve, reject) => {
-      const queryStatement = `UPDATE answer SET reported = 1 WHERE answer_id = ${answer_id};`;
+      const queryStatement = `UPDATE answer SET reported = true WHERE answer_id = ${answer_id};`;
       pool.query(queryStatement, (err, results) => {
         if (err) {
           reject(err);
