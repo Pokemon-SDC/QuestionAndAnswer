3b1d2b8be773f28a67d19e9ce70a2aebfe2a7838
Brandon Hsu <brandon.j.hsu@gmail.com>
2022-07-31 20:06:57 -0700

    implemented Indexes to improve query times

--- database.sql
+++ database.sql
@@ -29,3 +29,18 @@ CREATE TABLE photo (
 COPY question FROM '/Users/brandonhsu/Desktop/SDC/csv_files/questions.csv' DELIMITER ',' CSV HEADER;
 COPY answer FROM '/Users/brandonhsu/Desktop/SDC/csv_files/answers.csv' DELIMITER ',' CSV HEADER;
 COPY photo FROM '/Users/brandonhsu/Desktop/SDC/csv_files/answers_photos.csv' DELIMITER ',' CSV HEADER;
+
+
+CREATE INDEX question_id_idx ON question (id);
+CREATE INDEX question_product_id_idx ON question (product_id);
+CREATE INDEX question_helpfulness_idx ON question (helpfulness);
+CREATE INDEX question_reported_idx ON question (reported);
+
+
+CREATE INDEX answer_answer_id_idx ON answer (answer_id);
+CREATE INDEX anwer_helpful_idx ON answer (helpful);
+CREATE INDEX answer_reported_idx ON answer (reported);
+CREATE INDEX answer_question_id_idx ON answer (question_id);
+
+CREATE INDEX photo_id_idx ON photo (id);
+CREATE INDEX photo_answer_id_idx ON photo (answer_id);
