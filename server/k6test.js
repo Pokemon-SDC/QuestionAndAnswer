4e8a5c0ffbd31f81051470c3169d394ca910a448
Brandon Hsu <brandon.j.hsu@gmail.com>
2022-08-02 18:11:16 -0700

    updated k6 stress test

--- server/k6test.js
+++ server/k6test.js
@@ -1,19 +1,43 @@
+/* eslint-disable import/no-unresolved */
 import http from 'k6/http';
 import { sleep } from 'k6';
 
 export const options = {
 
   stages: [
-    { duration: '30s', target: 100 },
-    { duration: '1m', target: 200 },
-    { duration: '1m', target: 400 },
-    { duration: '2m', target: 500 },
-    { duration: '1m', target: 800 },
-    { duration: '2m', target: 0 },
+    { duration: '10s', target: 100 },
+    { duration: '10s', target: 200 },
+    { duration: '10s', target: 300 },
+    { duration: '10s', target: 100 },
+    { duration: '10s', target: 0 },
   ],
+
 };
 
 export default () => {
-  http.put('http://localhost:3000/qa/questions/9000/helpful');
+  // POST STRESS TEST
+
+  const randomProdID = Math.floor(Math.random() * 1000000);
+  const randomQAID = Math.floor(Math.random() * 10000);
+
+  const params = {
+    body: 'i hope this works!',
+    name: 'the one and only',
+    email: 'hahahah',
+    product_id: randomProdID,
+  };
+
+  http.post('http://localhost:3000/qa/questions', JSON.stringify(params), {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  // GET STRESS TEST
+  http.get(`http://localhost:3000/qa/questions/?product_id=${randomProdID}`);
+
+  // PUT STRESS TEST
+  http.put(`http://localhost:3000/qa/questions/${randomQAID}/helpful`);
+
   sleep(1);
 };
